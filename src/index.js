c0f2817e564158410fc82c3bfab737d2bdff3162
Berelindis <romain.bellande@orange.fr>
2017-10-03 00:01:48 +0200

    update search-bar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 
 const API_KEY = 'AIzaSyBA9XgX2ANybTXD_lrMmxyb6OdSzDelNcM';
 
+import SearchBar from './components/search-bar';
+
 class App extends Component {
   render() {
     return (
       <div>
-        App
+        <SearchBar />
       </div>
     );
   }
