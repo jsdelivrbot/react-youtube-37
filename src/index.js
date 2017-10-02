7ee1d2f24656bd94cd0ba72b3c2e581e7da532cc
Berelindis <romain.bellande@orange.fr>
2017-10-02 23:36:33 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyBA9XgX2ANybTXD_lrMmxyb6OdSzDelNcM';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        App
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
